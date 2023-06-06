a90d539b5b1a9143df23c7b9ef29985b6e55bd7a
MissJuly <joygitau98@gmail.com>
2023-06-06 22:36:25 +0300

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 \
+                Safari/537.36"
+    }
+    param = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, param=param, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(top.get("data").get("title")) for top in results
+     .get("children")]
